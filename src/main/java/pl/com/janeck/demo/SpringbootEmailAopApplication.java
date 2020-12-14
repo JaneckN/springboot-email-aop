4d11caf6dcbdfbd70146c56393747853997544ca
Jan Nowak <janeck@protonmail.com>
2020-12-14 20:44:06 +0000

    first commit

---
+++ src/main/java/pl/com/janeck/demo/SpringbootEmailAopApplication.java
@@ -0,0 +1,13 @@
+package pl.com.janeck.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootEmailAopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootEmailAopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/com/janeck/demo/SpringbootEmailAopApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.com.janeck.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootEmailAopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
